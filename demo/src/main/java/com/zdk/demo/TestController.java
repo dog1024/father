9de4c5e2ab638d517b9839eda15c2537b23aca7a
dog1024 <406107277@qq.com>
2019-07-22 15:02:50 +0800

    修改配置

---
+++ demo/src/main/java/com/zdk/demo/TestController.java
@@ -0,0 +1,20 @@
+package com.zdk.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+//这里面的属性有可能会更新的，git中的配置中心变化的话就要刷新，没有这个注解内，配置就不能及时更新
+@RefreshScope
+public class TestController {
+
+    @Value("${myname}")
+    private String name;
+
+    @RequestMapping("/test")
+    public String test(){
+        return this.name;
+    }
+}
